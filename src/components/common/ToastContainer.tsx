14add41b603fde897c281d94d5b2acb0c53cd2fb
plconsultingpaul <159668820+plconsultingpaul@users.noreply.github.com>
2025-12-05 21:03:43 -0800

    Sync

---
+++ src/components/common/ToastContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Toast, { ToastProps } from './Toast';
+
+interface ToastContainerProps {
+  toasts: ToastProps[];
+  onClose: (id: string) => void;
+}
+
+export default function ToastContainer({ toasts, onClose }: ToastContainerProps) {
+  return (
+    <div className="fixed top-4 right-4 z-50 flex flex-col space-y-2 pointer-events-none">
+      {toasts.slice(0, 3).map((toast) => (
+        <div key={toast.id} className="pointer-events-auto">
+          <Toast {...toast} onClose={onClose} />
+        </div>
+      ))}
+    </div>
+  );
+}
