3e824179e3cf8a8b945f3b7d961f71d5006c6c88
plconsultingpaul <159668820+plconsultingpaul@users.noreply.github.com>
2026-02-05 07:14:08 -0600

    Sync

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
+  safelist: [
+    'col-span-1', 'col-span-2', 'col-span-3', 'col-span-4', 'col-span-5', 'col-span-6',
+    'col-span-7', 'col-span-8', 'col-span-9', 'col-span-10', 'col-span-11', 'col-span-12',
+    'md:col-span-1', 'md:col-span-2', 'md:col-span-3', 'md:col-span-4', 'md:col-span-5', 'md:col-span-6',
+    'md:col-span-7', 'md:col-span-8', 'md:col-span-9', 'md:col-span-10', 'md:col-span-11', 'md:col-span-12',
+  ],
   theme: {
   	extend: {
   		colors: {
